51f641e42295a042cf67524a05fe5bb9cc5e84f0
Endert Guerrero <e.guerrerocamacho@uandresbello.edu>
2025-05-13 17:55:09 -0400

    Actualiza la configuración de Tailwind para cambiar el color primario a cian, modifica el texto en la sección "Sobre mi" y elimina componentes obsoletos como "Expertise" y "Workshops". Se añaden nuevos componentes de "Services" y "Contact" para mejorar la funcionalidad del sitio.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
       current: 'currentColor',
       transparent: 'transparent',
       white: '#ffffff',
-      primary: colors.stone,
+      primary: colors.cyan,
     },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
