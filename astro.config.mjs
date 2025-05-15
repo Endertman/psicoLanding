cd6a401e940a1d5c7c8c128d11363b5f8186e263
Endert Guerrero <e.guerrerocamacho@uandresbello.edu>
2025-05-14 23:17:59 -0400

    Se actualiza la configuración de Astro para definir la salida como estática y se establece el directorio de construcción como `dist`. Se elimina la referencia a `dist/` en el archivo `.gitignore` para permitir su seguimiento.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
-// https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
   integrations: [tailwind(), compress()],
+  output: 'static',
+  build: {
+    outDir: 'dist',
+  },
+  base: '/psicoLanding/', 
 });
