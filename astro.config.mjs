51f6cfd34f6b7c46b6113c813a346ae59a9ab37c
Endert Guerrero <e.guerrerocamacho@uandresbello.edu>
2025-05-27 14:40:59 -0400

    Actualiza la configuración de Astro para habilitar las transiciones de vista y actualiza las dependencias en `package.json` y `package-lock.json` a versiones más recientes, incluyendo mejoras en Tailwind y otras bibliotecas.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+// https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
+  viewTransitions: true,
   integrations: [tailwind(), compress()],
-  output: 'static',
-  build: {
-    outDir: 'dist',
-  },
-  base: '/', 
 });
